d1c5e2782967cd40a8a2e466ca2ba3cf30924011
Tertiium <yang.guozhang@yahoo.com>
2017-01-27 13:53:52 -0200

    Primeiro Commit

---
+++ MovileCarthage/MovileCarthage/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  MovileCarthage
+//
+//  Created by user on 11/01/17.
+//  Copyright © 2017 user. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    //MARK: - Outlets
+    
+    //MARK: - Properties
+    
+    //MARK: - Super Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        /*
+        Alamofire.request("https://private-35178c-trakt.apiary-mock.com/shows/popular").responseJSON { response in
+            //debugPrint(response)
+            
+            if let json = response.result.value {
+                print("JSON: \(json)")
+            }
+        }*/
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    //MARK: - Actions
+    
+    
+}
+
